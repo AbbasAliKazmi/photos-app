82181765d44de346b9c13c4e96d363fd5b7bd22c
AbbasAliKazmi <syedabbasali1998.aa@gmail.com>
2024-01-23 05:08:42 +0500

    Add dependencies and update code for image upload and display

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,36 @@
 'use client';
 
-import { CldUploadButton } from 'next-cloudinary';
+import { CldImage, CldUploadButton } from 'next-cloudinary';
+import { useState } from 'react';
+type UploadResult = {
+  info:{
+     public_id: string,
+  },
+  event: 'success'
+}
 
 export default function Home() {
+
+  const [imageId, setImageId] = useState('')
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <CldUploadButton uploadPreset="axoxh59b" />
+      <CldUploadButton 
+        onUpload={(result:UploadResult)=>{
+        setImageId(result.info.public_id);
+      }} 
+      uploadPreset="axoxh59b" 
+      />
+      
+
+      {imageId && (
+      <CldImage
+        width="400"
+        height="300"
+        src={imageId}
+        sizes="100vw"
+        alt="Description of my image"
+      />
+  )}
     </main>
   );
 }
